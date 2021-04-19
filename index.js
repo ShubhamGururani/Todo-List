0e834ab08752081c27ce5b8d1b14a0d7072e4ce8
ShubhamGururani <shubhamgururani@gmail.com>
2021-04-19 14:36:28 +0530

    code: setup view engine and basic home layout

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 // require express to setup server
 const express = require('express');
-
+// give the port number 
+const port = 8000;
 // run the express function
 const app = express();
 
-// give the port number 
-const port = 8000;
+// setting up view engine
+app.set('view engine', 'ejs');
+// giving it the folder in which to look for views
+app.set('views', './views');
 
-// make the app listen
+// temporary storage( remove it later on)
+var todoList = [{
+    topic: "Buy Cheese",
+}, {
+    topic: "Buy Toppings"
+}, {
+    topic: "Make Pizza"
+}]
+
+// define routes
+app.get('/', function(req, res) {
+    return res.render('home', {
+        title: "Todo",
+        todo_list: todoList
+    });
+});
+
+// make the app listen on the given port number
 app.listen(port, function(err) {
     if (err) {
         // if error occurs this message is printed
